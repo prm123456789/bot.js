6a18c547b6615c4ac1cc1f0cae6f17220587c2fb
prm123456789 <guerrierjojo8@gmail.com>
2025-07-03 09:08:06 -0300

    Create menu.js

---
+++ WhatsApp/menu.js
@@ -0,0 +1,10 @@
+module.exports = async (conn, m) => {
+  await m.reply(`
+╭⭑━━━➤ 🤖 INCONNU XD V2 MENU
+┣ ◁️  !menu : Voir ce menu
+┣ ◁️  !ping : Vérifier la latence
+┣ ◁️  !botinfo : Infos du bot
+┣ ◁️  !kick <numéro> : Expulser un membre
+┣ ◁️  !kickall : Expulser tout le monde (admin seulement)
+╰━━━━━━━━━━━━━━━━━━━╯`);
+};
